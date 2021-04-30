94ab9a252f3bcfefb38b60b2e340b49e3ffc0559
thewongian <thewongian@vt.edu>
2021-04-30 17:36:44 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/painter/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.painter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button start;
+    public static final int REQUEST_IMAGE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.start);
+
+        start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //start camera
+        if (v.getId() == R.id.start) {
+            Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(takePicIntent.resolveActivity(getPackageManager()) != null){
+                startActivityForResult(takePicIntent, REQUEST_IMAGE);
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap thumbnail = (Bitmap) extras.get("data");
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/painter/PaintingActivity.java
@@ -0,0 +1,14 @@
+package com.example.painter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaintingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_painting);
+    }
+}
